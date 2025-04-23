91b22109e0d63fb5800064cc21f9e037f531706b
ZhilanMuhammad <zhilanmuhammad11@gmail.com>
2025-04-23 21:19:56 +0700

    💄 update detail mobil

--- resources/views/landingPage/detailMobil.blade.php
+++ resources/views/landingPage/detailMobil.blade.php
@@ -149,144 +149,43 @@
 
                         <div class="tab-pane fade show active" id="pills-review" role="tabpanel"
                             aria-labelledby="pills-review-tab">
-                            <div class="row">
-                                <div class="col-md-7">
-                                    <h3 class="head">23 Reviews</h3>
-                                    <div class="review d-flex">
-                                        <div class="user-img"
-                                            style="background-image: url('{{asset('ladingPage/images/person_1.jpg')}}');">
-                                        </div>
-                                        <div class="desc">
-                                            <h4>
-                                                <span class="text-left">Jacob Webb</span>
-                                                <span class="text-right">14 March 2018</span>
-                                            </h4>
-                                            <p class="star">
-                                                <span>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                </span>
-                                                <span class="text-right"><a href="#" class="reply"><i
-                                                            class="icon-reply"></i></a></span>
-                                            </p>
-                                            <p>When she reached the first hills of the Italic Mountains, she had a last
-                                                view back on the skyline of her hometown Bookmarksgrov</p>
-                                        </div>
+                            <div class="container">
+                                <h3 class="head mb-4">Formulir Pemesanan</h3>
+                                <form>
+                                    <div class="form-group">
+                                        <label for="nama">Nama</label>
+                                        <input type="text" class="form-control" id="nama" value="Alif" disabled>
                                     </div>
-                                    <div class="review d-flex">
-                                        <div class="user-img"
-                                            style="background-image: url('{{asset('ladingPage/images/person_2.jpg')}}');">
-                                        </div>
-                                        <div class="desc">
-                                            <h4>
-                                                <span class="text-left">Jacob Webb</span>
-                                                <span class="text-right">14 March 2018</span>
-                                            </h4>
-                                            <p class="star">
-                                                <span>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                </span>
-                                                <span class="text-right"><a href="#" class="reply"><i
-                                                            class="icon-reply"></i></a></span>
-                                            </p>
-                                            <p>When she reached the first hills of the Italic Mountains, she had a last
-                                                view back on the skyline of her hometown Bookmarksgrov</p>
-                                        </div>
+                                    <div class="form-group">
+                                        <label for="no_hp">No Handphone</label>
+                                        <input type="text" class="form-control" id="no_hp" value="081291226574"
+                                            disabled>
                                     </div>
-                                    <div class="review d-flex">
-                                        <div class="user-img" style="background-image: url(images/person_3.jpg)"></div>
-                                        <div class="desc">
-                                            <h4>
-                                                <span class="text-left">Jacob Webb</span>
-                                                <span class="text-right">14 March 2018</span>
-                                            </h4>
-                                            <p class="star">
-                                                <span>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                </span>
-                                                <span class="text-right"><a href="#" class="reply"><i
-                                                            class="icon-reply"></i></a></span>
-                                            </p>
-                                            <p>When she reached the first hills of the Italic Mountains, she had a last
-                                                view back on the skyline of her hometown Bookmarksgrov</p>
-                                        </div>
+                                    <div class="form-group">
+                                        <label for="tanggal">Tanggal Mulai</label>
+                                        <input type="date" class="form-control" id="tanggal">
                                     </div>
-                                </div>
-                                <div class="col-md-5">
-                                    <div class="rating-wrap">
-                                        <h3 class="head">Give a Review</h3>
-                                        <div class="wrap">
-                                            <p class="star">
-                                                <span>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    (98%)
-                                                </span>
-                                                <span>20 Reviews</span>
-                                            </p>
-                                            <p class="star">
-                                                <span>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    (85%)
-                                                </span>
-                                                <span>10 Reviews</span>
-                                            </p>
-                                            <p class="star">
-                                                <span>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    (70%)
-                                                </span>
-                                                <span>5 Reviews</span>
-                                            </p>
-                                            <p class="star">
-                                                <span>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    (10%)
-                                                </span>
-                                                <span>0 Reviews</span>
-                                            </p>
-                                            <p class="star">
-                                                <span>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    <i class="ion-ios-star"></i>
-                                                    (0%)
-                                                </span>
-                                                <span>0 Reviews</span>
-                                            </p>
-                                        </div>
+                                    <div class="form-group">
+                                        <label for="tanggal">Tanggal Selesai</label>
+                                        <input type="date" class="form-control" id="tanggal">
                                     </div>
-                                </div>
+                                    <div class="form-group">
+                                        <label for="waktu">Jumlah Hari</label>
+                                        <input type="number" class="form-control" id="hari">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tipe">Pilih Jenis Pembayaran</label>
+                                        <select class="form-control" id="tipe">
+                                            <option value="">-- Pilih Pembayaran --</option>
+                                            <option value="cash">Cash</option>
+                                            <option value="ciciilan">Cicilan</option>
+                                        </select>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary mt-3">Kirim</button>
+                                </form>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
--- resources/views/landingPage/mobil.blade.php
+++ resources/views/landingPage/mobil.blade.php
@@ -55,40 +55,4 @@
     </div>
 </section>
 
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Formulir Pemesanan</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form>
-                    <div class="form-group">
-                        <label for="nama">Nama</label>
-                        <input type="text" class="form-control" id="nama" placeholder="Masukkan nama Anda">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="Masukkan email Anda">
-                    </div>
-                    <div class="form-group">
-                        <label for="tanggal">Tanggal Pemesanan</label>
-                        <input type="date" class="form-control" id="tanggal">
-                    </div>
-                    <div class="form-group">
-                        <label for="waktu">Waktu Pemesanan</label>
-                        <input type="time" class="form-control" id="waktu">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Kirim</button>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
